22b0af3b470f37d6fed82c0583d53ae33e4ecd1f
Christian Zelaya <christian@cerouno.com.py>
2019-09-03 14:54:24 -0400

    Update v1.30

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 	require __DIR__.'/../app/get.php';			//SOLICITUD GET
 	require __DIR__.'/../app/post.php';			//SOLICITUD POST
+	require __DIR__.'/../app/report.php';		//SOLICITUD REPORT
 //	require __DIR__.'/../app/put.php';			//SOLICITUD PUT
 //	require __DIR__.'/../app/delete.php';		//SOLICITUD DELETE
