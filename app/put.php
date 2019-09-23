48189db6354ad85174a88ede464b20a2cc39afdc
Christian Zelaya <christian@cerouno.com.py>
2019-09-23 09:14:36 -0400

    Update v1.46

--- app/put.php
+++ app/put.php
@@ -5,28 +5,25 @@
         $val00      = $request->getAttribute('codigo');
         
         if (isset($val00)) {
-            $sql00  = "UPDATE COMPCAB SET COMPCABCCI = COMPCABCCI + 1 WHERE COMPCABCOD = ?";
+            $sql    = "UPDATE COMPCAB SET COMPCABCCI = COMPCABCCI + 1 WHERE COMPCABCOD = ?";
+            $parm   = array($val00);
+            $stmt   = sqlsrv_query($mssqlConn, $sql, $parm);
 
-            try {
-                $connMYSQL  = getConnectionMYSQL();
-                $stmtMYSQL  = $connMYSQL->prepare($sql00);
-                $stmtMYSQL->execute([$val00]); 
-                
+            if ($stmt === FALSE) {
                 header("Content-Type: application/json; charset=utf-8");
-                $json       = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success UPDATE', 'codigo' => $val00), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
-
-                $stmtMYSQL->closeCursor();
-                $stmtMYSQL = null;
-            } catch (PDOException $e) {
+                $json = json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Hubo un error al momento de ACTUALIZAR', 'erros' => sqlsrv_errors()), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            } else {
                 header("Content-Type: application/json; charset=utf-8");
-                $json = json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error UPDATE: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+                $json = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success UPDATE'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
             }
+
+            sqlsrv_free_stmt($stmt);
         } else {
             header("Content-Type: application/json; charset=utf-8");
             $json = json_encode(array('code' => 400, 'status' => 'error', 'message' => 'Verifique, algún campo esta vacio.'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
         }
 
-        $connMYSQL  = null;
+        sqlsrv_close($mssqlConn);
         
         return $json;
     });
